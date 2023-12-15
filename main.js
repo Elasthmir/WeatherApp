b6c198c3cda048e5e2736b307495d4dbb6d42de5
A.Jamrowski <adrian.jamrowski@interia.pl>
2023-12-15 06:36:43 +0100

    4 commit

--- main.js
+++ main.js
@@ -31,17 +31,18 @@ const apiLang1 = '&lang=pl';
 
 const cloudsCover = document.querySelector('.cloudsCover');
 
+var airQuality = '';
 
-//grid 
-
-const div1 = document.querySelector(".div1");
-const div2 = document.querySelector(".div2");
-const div3 = document.querySelector(".div3");
-const div4 = document.querySelector(".div4");
-const div5 = document.querySelector(".div5");
-const div6 = document.querySelector(".div6");
-const div7 = document.querySelector(".div7");
-const div8 = document.querySelector(".div8");
+//grid  
+const parent = document.querySelector(".parent");
+const weatherDescription = document.querySelector(".div1");
+const weatherPressure = document.querySelector(".div2");
+const weatherHumidity = document.querySelector(".div3");
+const weatherWind = document.querySelector(".div4");
+const weatherRains = document.querySelector(".div5");
+const weatherVisibility = document.querySelector(".div6");
+const weatherP25 = document.querySelector(".div7");
+const weatherWindSpeed = document.querySelector(".div8");
 const div9 = document.querySelector(".div9");
 
 
@@ -49,29 +50,33 @@ const div9 = document.querySelector(".div9");
 function getWeather() {
 
     const apiCity = input.value;
-    const url = `https://geocoding-api.open-meteo.com/v1/search?name=${apiCity}&count=100&language=pl&format=json`;
+    const url = `https://geocoding-api.open-meteo.com/v1/search?name=${apiCity}&count=10&language=en&format=json`;
 
     axios.get(url).then(response => {
         let cordsArrayLat = [];
         let cordsArrayLon = [];
-        console.log(response);
+        let time = [];
+        //console.log(response);
         if (response.data.results == undefined) {
 
             townResults.textContent = "Nie ma takiego miasta!!!"
-            console.log('error')
+            //console.log('error')
 
         } else {
             response.data.results.forEach(element => {
                 if (element.name !== apiCity) { return; }
-                if (element.admin3 == undefined) { return; }
+                if (element.admin3 == undefined) { element.admin3 = ""; }
 
                 cordsArrayLat.push(element.latitude);
                 cordsArrayLon.push(element.longitude);
+                time.push(element.timezone);
 
             })
             arrayCounter = 0
             response.data.results.forEach(element => {
-
+                input.classList.add("fadeOutElements")
+                button.classList.add("fadeOutElements")
+                //  parent.style.display = 'none'
                 if (element.name !== apiCity) { return; }
                 // if (element.country_code !== "PL") { return; }
                 if (element.admin3 == undefined) { return; }
@@ -142,7 +147,7 @@ function getWeather() {
                         $items.forEach((el, i) => {
                             const x = `${i * itemWidth + scroll}`;
                             const s = wrap(-itemWidth * 16, (wrapWidth - itemWidth) * 2.4, parseInt(x));
-                            el.style.transform = `translate(${s}px, 0px)`;
+                            el.style.transform = `translate(${s * 1.5}px, 0px)`;
                         });
                     };
                     dispose(0);
@@ -206,7 +211,7 @@ function getWeather() {
                 }
             });
             const nameTown = document.querySelectorAll('.eachTown');
-            console.log(nameTown.length)
+            //console.log(nameTown.length)
             for (let index = 0; index <= nameTown.length; index++) {
                 nameTown[index].addEventListener("click", function () {
                     $("#townResults").fadeOut(500, function () {
@@ -220,18 +225,18 @@ function getWeather() {
 
 
                     setTimeout(function () {
-                        firstDiv.style.display = "none";
+                        firstDiv.style.opacity = "0";
                         secondDiv.style.transition = "opacity 0.5s";
                         secondDiv.style.opacity = 1;
                     }, 500);
 
 
-                    console.log(cordsArrayLat[index], cordsArrayLon[index])
+                    //console.log(cordsArrayLat[index], cordsArrayLon[index], time[index])
                     const weatherURL = `https://api.open-meteo.com/v1/forecast?latitude=${cordsArrayLat[index]}&longitude=${cordsArrayLon[index]}&hourly=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,rain,showers,snowfall,snow_depth,weather_code,surface_pressure,cloud_cover,visibility,wind_speed_10m,wind_direction_10m`
 
                     axios.get(weatherURL).then(responseWeather => {
 
-                        console.log(responseWeather);
+                        //console.log(responseWeather);
 
 
                         let d = new Date();
@@ -241,20 +246,178 @@ function getWeather() {
 
                         imgApi = apiLink + apiCity + apiKey + apiUnits + apiLang
                         axios.get(imgApi).then(imgApi => {
-                            console.log(imgApi)
-                            // img.src = `https://openweathermap.org/img/wn/${imgApi.data.weather[0].icon}.png`;
-                            div1.innerHTML += `<div>
-                                                    <img src='https://openweathermap.org/img/wn/${imgApi.data.weather[0].icon}.png'/>
-                                                    <br/>
-                                                    ${feels_like.textContent = responseWeather.data.hourly.temperature_2m[d]} &#176;C
-                                                    <br/>
-                                                    ${imgApi.data.weather[0].description}
-                                                    <br/>
-                                                    
-                                                    ${responseWeather.data.hourly.time[d]}<br/>
-                                                    Temperatura odczuwalna:  ${responseWeather.data.hourly.apparent_temperature[d]} &#176;C <br/>
-                                                    Zachmurzenie: ${responseWeather.data.hourly.cloud_cover[d]} %
-                                                </div>`
+                            //console.log(imgApi)
+
+                            //d = d + timeZonesArray[time[index]]
+
+                            //console.log(time[index])
+
+
+
+                            function getTimezoneOffset(timeZone) {
+                                const now = new Date();
+                                const tzString = now.toLocaleString('en-US', { timeZone });
+                                const localString = now.toLocaleString('en-US');
+                                const diff = (Date.parse(localString) - Date.parse(tzString)) / 3600000;
+                                const offset = diff + now.getTimezoneOffset() / 60;
+
+                                return -offset;
+                            }
+                            let windDescription;
+                            let array;
+                            const offset = getTimezoneOffset(time[index]);
+                            if (Math.round(responseWeather.data.hourly.wind_direction_10m[d]) <= 45) {
+
+                                windDescription = 'Południowy'
+                                array = '&#8593;'
+
+                            }
+
+                            if (Math.round(responseWeather.data.hourly.wind_direction_10m[d]) > 45 && Math.round(responseWeather.data.hourly.wind_direction_10m[d]) < 90) {
+
+                                windDescription = 'Południowo-Zachodni'
+                                array = '&#8599;'
+
+                            }
+                            if (Math.round(responseWeather.data.hourly.wind_direction_10m[d]) > 90 && Math.round(responseWeather.data.hourly.wind_direction_10m[d]) < 125) {
+
+                                windDescription = 'Wschodni'
+                                array = '&#8594;'
+                            }
+                            if (Math.round(responseWeather.data.hourly.wind_direction_10m[d]) > 125 && Math.round(responseWeather.data.hourly.wind_direction_10m[d]) < 180) {
+
+                                windDescription = 'Północno-Zachodni';
+                                array = '&#8600;';
+
+                            }
+                            if (Math.round(responseWeather.data.hourly.wind_direction_10m[d]) >= 180 && Math.round(responseWeather.data.hourly.wind_direction_10m[d]) < 225) {
+
+                                windDescription = 'Północny'
+                                array = '&#8595;'
+                            }
+                            if (Math.round(responseWeather.data.hourly.wind_direction_10m[d]) >= 225 && Math.round(responseWeather.data.hourly.wind_direction_10m[d]) < 270) {
+
+                                windDescription = 'Północno-Wschodni'
+                                array = '&#8601;'
+
+
+                            }
+                            if (Math.round(responseWeather.data.hourly.wind_direction_10m[d]) >= 270 && Math.round(responseWeather.data.hourly.wind_direction_10m[d]) < 315) {
+
+                                windDescription = 'Wschodni'
+                                array = '&#8592;'
+
+
+
+                            }
+                            if (Math.round(responseWeather.data.hourly.wind_direction_10m[d]) >= 315 && Math.round(responseWeather.data.hourly.wind_direction_10m[d]) < 335) {
+
+                                windDescription = 'Południowo-Wschodni'
+                                array = '&#8598;'
+
+
+
+                            }
+                            if (Math.round(responseWeather.data.hourly.wind_direction_10m[d]) >= 335 && Math.round(responseWeather.data.hourly.wind_direction_10m[d]) < 360) {
+
+                                windDescription = 'Południowy'
+                                array = '&#8598;'
+
+
+
+                            }
+
+
+
+
+
+
+                            //console.log(offset);
+                            parent.classList.add("fadeInElements");
+                            d = d + offset - 1
+                            axios.get(`https://air-quality-api.open-meteo.com/v1/air-quality?latitude=${cordsArrayLat[index]}&longitude=${cordsArrayLon[index]}&hourly=pm2_5`).then(responseAir => {
+
+                                airQuality += responseAir.data.hourly.pm2_5[d]
+                                console.log(responseAir.data.hourly.pm2_5[d])
+                                // img.src = `https://openweathermap.org/img/wn/${imgApi.data.weather[0].icon}.png`;
+                                weatherDescription.innerHTML += `
+                            <div class = 'firstDiv'>
+                                <div class='firstPart'>
+                                
+                                <br/>
+                                Temperatura<br/><span class = 'values'>${feels_like.textContent = Math.round(responseWeather.data.hourly.temperature_2m[d])} </span>&#176;C
+                                <br/>
+                                Odczuwalna: <br/> <span class = 'values'>${Math.round(responseWeather.data.hourly.apparent_temperature[d])} </span>&#176;C <br/>
+                                <br/>
+                               
+                                </div>
+                                <div class='secondPart'>
+                                    ${imgApi.data.weather[0].description}<br/>
+                                    Zachmurzenie:<br/> <span class = 'values'>${responseWeather.data.hourly.cloud_cover[d]}</span> %
+                                </div>
+                            </div>`
+
+                                weatherPressure.innerHTML += `<div>
+                                Ciśnienie<br/>
+                                <span class = 'values'>${Math.round(responseWeather.data.hourly.surface_pressure[d])}</span>
+                                <br/>
+                                <span>hPa</span>
+                            <div>`
+
+
+                                weatherHumidity.innerHTML += `<div>
+                                Wilgotność<br/>
+                                <span class = 'values'>${responseWeather.data.hourly.relative_humidity_2m[d]}</span>
+                                <br/>%
+                            <div>`
+                                weatherWind.innerHTML += `<div>
+                                Wiatr<br/>
+                                <span class = 'values'>${Math.round(responseWeather.data.hourly.wind_speed_10m[d])}</span>
+                                <br/>
+                                <span>km/h</span>
+                            <div>`
+                                weatherRains.innerHTML += `<div>Opady: 
+                                    <br/> 
+                                    deszcz:<span class = 'values'>${Math.round(responseWeather.data.hourly.rain[d])}</span>mm śnieg:
+                                    <span class = 'values'>${Math.round(responseWeather.data.hourly.snowfall[d])}</span>mm
+                                    <br/>
+                                    <center>Warstwa śniegu<br/><span class = 'values'>${Math.round(responseWeather.data.hourly.snow_depth[d])}</span>mm</center>
+                                    
+                            <div>`
+                                weatherVisibility.innerHTML += `<div>
+                                Widoczność<br/><span class = 'values'>${Math.round(responseWeather.data.hourly.visibility[d] / 1000)}</span>
+                                <br/>
+                                <span>Km</span>
+                            <div>`
+                                weatherP25.innerHTML += `<div>
+                                Zanieczyszczenie<br/>
+                                <span class = 'values'>${responseAir.data.hourly.pm2_5[d]}</span>
+                                <br/>
+                                <span>&#x3BC;g/m&#179;  </span>
+                            <div>`
+                                weatherWindSpeed.innerHTML += `<div>
+                                Kierunek wiatru: <br/>
+                                <span class = 'values'>${array}<br/>${windDescription}</span>
+                                <br/>
+                                
+                            <div>`
+
+
+
+
+                                const backButton = document.querySelector(".back");
+                                backButton.addEventListener("click", () => {
+
+
+                                    location.reload()
+
+                                })
+
+
+                            })
+
+
+
 
                         })
 
@@ -296,7 +459,7 @@ function getWeather() {
 
     }).catch(error => {
 
-        console.log(error);
+        //console.log(error);
         if (error.response.data.results == undefined) {
 
             errorMsg.textContent = `${error.response.data.message}`;
@@ -323,7 +486,7 @@ const getWeatherByEnter = (e) => {
     if (e.key === 'Enter') {
 
         getWeather();
-        townResults.innerHTML = "";
+        townResults.innerHTML += "";
 
     }
 
